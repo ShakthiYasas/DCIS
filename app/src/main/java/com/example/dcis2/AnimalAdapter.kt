5efe3cd98445dcd2f8bd996182626707450ffc28
Mach Dieu Bang <machdieubang2110@gmail.com>
2024-11-13 15:59:54 +1100

    Add Location Exception case, drawable icon for animal perferences

--- app/src/main/java/com/example/dcis2/AnimalAdapter.kt
+++ app/src/main/java/com/example/dcis2/AnimalAdapter.kt
@@ -1,5 +1,3 @@
-package com.example.dcis2
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -7,34 +5,27 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.dcis2.Model.AnimalCategory
+import com.example.dcis2.R
 
-class AnimalAdapter(
-    private val context: Context,
-    private val animalNames: Array<String>,
-    private val animalImages: Array<Int>
-) : BaseAdapter() {
+class AnimalAdapter(private val context: Context, private val categories: List<AnimalCategory>) : BaseAdapter() {
 
-    override fun getCount(): Int = animalNames.size
+    override fun getCount(): Int = categories.size
 
-    override fun getItem(position: Int): Any = animalNames[position]
+    override fun getItem(position: Int): Any = categories[position]
 
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.grid_item_animal, parent, false)
+        val category = categories[position]
 
-        // Initialize the button and set up the click listener
-        val view = convertView ?: LayoutInflater.from(context).inflate(
-            R.layout.grid_item_animal, parent, false
-        )
+        val imageView = view.findViewById<ImageView>(R.id.animalImage)
+        val textView = view.findViewById<TextView>(R.id.animalText)
 
-        val imageView: ImageView = view.findViewById(R.id.animal_image)
-        val textView: TextView = view.findViewById(R.id.animal_name)
-
-        imageView.setImageResource(animalImages[position])
-        textView.text = animalNames[position]
+        imageView.setImageResource(category.imageResId)
+        textView.text = category.name
 
         return view
     }
-
-
 }
