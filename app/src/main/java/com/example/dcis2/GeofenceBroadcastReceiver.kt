d90851053ffe1fa74dc3f327b505d51bdb288752
Mach Dieu Bang <machdieubang2110@gmail.com>
2024-12-14 01:49:56 +1100

    Update location of melbourne zoo

--- app/src/main/java/com/example/dcis2/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/example/dcis2/GeofenceBroadcastReceiver.kt
@@ -27,29 +27,110 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
         for (geofence in triggeringGeofences) {
             Log.d("GeofenceReceiver", "Triggered geofence: ${geofence.requestId}")
 
-            if (geofence.requestId == "oakland_zoo_enc") {
+            if (geofence.requestId == "Meerkats") {
                 when (geofenceTransition) {
                     Geofence.GEOFENCE_TRANSITION_ENTER -> {
-                        Log.d("GeofenceReceiver", "Entered Oakland Zoo")
-                        Toast.makeText(context, "Welcome to Oakland Zoo!", Toast.LENGTH_LONG).show()
+                        Log.d("GeofenceReceiver", "Entered Growing Wilds Areas")
+                        Toast.makeText(context, "Welcome to Growing Wilds Areas!", Toast.LENGTH_LONG).show()
                     }
 
                     Geofence.GEOFENCE_TRANSITION_EXIT -> {
-                        Log.d("GeofenceReceiver", "Exited Oakland Zoo")
-                        Toast.makeText(context, "You have exited Oakland Zoo.", Toast.LENGTH_LONG).show()
+                        Log.d("GeofenceReceiver", "Exited Growing Wilds Areas")
+                        Toast.makeText(context, "You have exited Growing Wilds Areas.", Toast.LENGTH_LONG)
+                            .show()
                     }
                 }
-            }
-            if (geofence.requestId == "Google_Head_quarter") {
+            } else if (geofence.requestId == "Penguins") {
                 // Handle Google Head Quarter geofence similarly
                 when (geofenceTransition) {
                     Geofence.GEOFENCE_TRANSITION_ENTER -> {
-                        Log.d("GeofenceReceiver", "Entered  Google Head Quarter")
+                        Log.d("GeofenceReceiver", "Entered  Penguins | Sea Creatures Area")
+                        Toast.makeText(context, "Welcome to Penguins Area!", Toast.LENGTH_LONG).show()
+                    }
+
+                    Geofence.GEOFENCE_TRANSITION_EXIT -> {
+                        Log.d("GeofenceReceiver", "Exited Penguins Area")
+                        Toast.makeText(context, "You have exited Penguins Area.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else if (geofence.requestId == "Lions") {
+                // Handle Google Head Quarter geofence similarly
+                when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("GeofenceReceiver", "Entered Lions Area")
+                        Toast.makeText(context, "Welcome to  Lions Area!", Toast.LENGTH_LONG).show()
+                    }
+
+                    Geofence.GEOFENCE_TRANSITION_EXIT -> {
+                        Log.d("GeofenceReceiver", "Exited Lions Area")
+                        Toast.makeText(context, "You have exited Lions Area", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }else if (geofence.requestId == "GiantsTortoises") {
+                // Handle Google Head Quarter geofence similarly
+                when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("GeofenceReceiver", "Entered  Giants Tortoises Area")
+                        Toast.makeText(context, "Welcome to Giants Tortoises Area!", Toast.LENGTH_LONG).show()
+                    }
+
+                    Geofence.GEOFENCE_TRANSITION_EXIT -> {
+                        Log.d("GeofenceReceiver", "Exited  Giants Tortoises Area")
+                        Toast.makeText(context, "You have exited  Giants Tortoises Area.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }else if (geofence.requestId == "Koalas") {
+                // Handle Google Head Quarter geofence similarly
+                when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("GeofenceReceiver", "Entered Koala Australia Native")
+                        Toast.makeText(context, "Welcome to Koala Australia Nativer!", Toast.LENGTH_LONG).show()
+                    }
+
+                    Geofence.GEOFENCE_TRANSITION_EXIT -> {
+                        Log.d("GeofenceReceiver", "Exited Koala Australia Native Area")
+                        Toast.makeText(context, "You have exited Koala Australia Native Area.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            else if (geofence.requestId == "Elephants") {
+                // Handle Google Head Quarter geofence similarly
+                when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("GeofenceReceiver", "Entered Elephant Area")
+                        Toast.makeText(context, "Welcome to Elephant Area!", Toast.LENGTH_LONG).show()
+                    }
+
+                    Geofence.GEOFENCE_TRANSITION_EXIT -> {
+                        Log.d("GeofenceReceiver", "Exited Elephant Area")
+                        Toast.makeText(context, "You have exited  Elephant Area.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            else if (geofence.requestId == "Orangutans") {
+                // Handle Google Head Quarter geofence similarly
+                when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("GeofenceReceiver", "Entered  Orangutans")
                         Toast.makeText(context, "Welcome to Google Head Quarter!", Toast.LENGTH_LONG).show()
                     }
 
                     Geofence.GEOFENCE_TRANSITION_EXIT -> {
-                        Log.d("GeofenceReceiver", "Exited  Google Head Quarter")
+                        Log.d("GeofenceReceiver", "Exited  Orangutans")
+                        Toast.makeText(context, "You have exited  Orangutans.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            else if (geofence.requestId == "Entrance&Exit") {
+                // Handle Google Head Quarter geofence similarly
+                when (geofenceTransition) {
+                    Geofence.GEOFENCE_TRANSITION_ENTER -> {
+                        Log.d("GeofenceReceiver", "Entered  Entrance & Exit")
+                        Toast.makeText(context, "Welcome to Google Head Quarter!", Toast.LENGTH_LONG).show()
+                    }
+
+                    Geofence.GEOFENCE_TRANSITION_EXIT -> {
+                        Log.d("GeofenceReceiver", "Exited  Entrance & Exit")
                         Toast.makeText(context, "You have exited  Google Head Quarter.", Toast.LENGTH_LONG).show()
                     }
                 }
--- app/src/main/java/com/example/dcis2/HealthSensorActivity.kt
+++ app/src/main/java/com/example/dcis2/HealthSensorActivity.kt
@@ -1,33 +1,14 @@
 package com.example.dcis2
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.PendingIntent
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import com.example.dcis2.ultility.LocationUtils
 import com.example.dcis2.utility.HealthSensorUtils
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.fitness.Fitness
-import com.google.android.gms.fitness.FitnessOptions
-import com.google.android.gms.fitness.data.DataType
-import com.google.android.gms.fitness.data.Field
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofenceStatusCodes
-import com.google.android.gms.location.GeofencingClient
-import com.google.android.gms.location.GeofencingRequest
-import com.google.android.gms.location.LocationServices
+
 
 class HealthSensorActivity : AppCompatActivity(), SensorEventListener {
 
@@ -36,7 +17,6 @@ class HealthSensorActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var heartRateTextView: TextView
     private lateinit var stepCountTextView: TextView
 
-ivate val LOCATION_PERMISSION_REQUEST_CODE = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_health_sensor)
@@ -57,51 +37,8 @@ ivate val LOCATION_PERMISSION_REQUEST_CODE = 1000
             println("Heart Rate Sensor not available.")
         }
         // Check and request location permissions
-        if (LocationUtils.checkAndRequestLocationPermission(this)) {
-            LocationUtils.fetchLocation(this) { latitude, longitude ->
-                Toast.makeText(this, "Location: $latitude, $longitude", Toast.LENGTH_LONG).show()
-            }
-        }
-        geofencingClient = LocationServices.getGeofencingClient(this)
 
-//        // Set up Oakland Zoo geofence
-        val geofenceList = listOf(
-            createGeofence("oakland_zoo_enc", 37.4220936, -122.083922, 60f) // Oakland Zoo geofence
-        )
-        val geofencingRequest = GeofencingRequest.Builder()
-            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-            .addGeofences(geofenceList)
-            .build()
 
-//        geofencingClient.addGeofences(geofencingRequest, geofencePendingIntent)
-//            .addOnSuccessListener {
-//                Log.d("Geofencing", "NEW Geofences added successfully")
-//            }
-//            .addOnFailureListener { exception ->
-//                if (exception is ApiException) {
-//                    // Handle the ApiException, e.g., check the status code
-//                    when (exception.statusCode) {
-//                        GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE -> {
-//                            // Location services might be disabled
-//                        }
-//                        // ... handle other status codes
-//                    }            }
-//
-//            }
-//        addGeofences(geofenceList)
-
-        geofenceList2 = ArrayList()
-        // Check permissions
-        if (hasLocationPermissions()) {
-            setupGeofences()
-        } else {
-            requestLocationPermissions()
-        }
-
-        // Example Geofence37.773628, -122.154722
-        geofenceList2.add(createGeofence("Home", 37.7749, -122.4194, 150f))
-        geofenceList2.add(createGeofence("oakland_zoo_enc", 37.773628, -122.154722, 60f))
-        geofenceList2.add(createGeofence("Google_Head_quarter", 37.4220936, -122.083922, 60f))
 
     }
     private fun fetchHealthData() {
@@ -148,113 +85,4 @@ ivate val LOCATION_PERMISSION_REQUEST_CODE = 1000
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         // Handle sensor accuracy changes if needed
     }
-
-    @SuppressLint("MissingPermission")
-    private fun addGeofences(geofences: List<Geofence>) {
-        if (checkPermissions()) {
-                geofencingClient.addGeofences(buildGeofencingRequest(geofences), geofencePendingIntent)
-                    .addOnSuccessListener {
-                        Log.d("Geofencing", "Geofences added successfully")
-                    }
-                    .addOnFailureListener { e ->
-                        Log.e("Geofencing", "Failed to add geofences", e)
-                    }
-        } else {
-            requestPermissions()
-        }
-    }
-    private fun checkPermissions(): Boolean {
-        return ActivityCompat.checkSelfPermission(
-            this, android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-            LOCATION_PERMISSION_REQUEST_CODE
-        )
-    }
-
-    private fun buildGeofencingRequest(geofences: List<Geofence>): GeofencingRequest {
-        return GeofencingRequest.Builder()
-            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-            .addGeofences(geofences)
-            .build()
-    }
-
-    private fun createGeofence(id: String, lat: Double, lng: Double, radius: Float): Geofence {
-        return Geofence.Builder()
-            .setRequestId(id)
-            .setCircularRegion(lat, lng, radius)
-            .setExpirationDuration(Geofence.NEVER_EXPIRE)
-            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
-            .build()
-    }
-
-    private fun getGeofencingRequest(): GeofencingRequest {
-        return GeofencingRequest.Builder()
-            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-            .addGeofences(geofenceList2)
-            .build()
-    }
-
-    private fun setupGeofences() {
-        if (!hasLocationPermissions()) {
-            Log.e("HealthSensorActivity", "Location permissions not granted")
-            return
-        }
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        geofencingClient.addGeofences(getGeofencingRequest(), geofencePendingIntent)
-            .addOnSuccessListener {
-                Log.i("HealthSensorActivity", "Geofences added successfully")
-            }
-            .addOnFailureListener { e ->
-                Log.e("HealthSensorActivity", "Failed to add geofences: ${e.message}")
-            }
-    }
-    private fun hasLocationPermissions(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-        } else {
-            TODO("VERSION.SDK_INT < Q")
-        }
-    }
-
-    private fun requestLocationPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                ),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-        }
-    }
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
-    }
-
-
-
-
 }
